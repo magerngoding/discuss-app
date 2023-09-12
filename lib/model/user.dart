ac89fba9e1799388b434c1cd1168d153aa32e2f8
Hafizh <Hafizh@gmail.com>
2023-09-12 10:05:17 +0700

    Add model for user, comment, topic

---
+++ lib/model/user.dart
@@ -0,0 +1,28 @@
+class User {
+  String id;
+  String username;
+  String password;
+  String image;
+  String createdAt;
+  String updatedAt;
+
+  User({
+    required this.id,
+    required this.username,
+    required this.password,
+    required this.image,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        id: json["id"],
+        username: json["username"],
+        password: json["password"],
+        image: json["image"],
+        createdAt: json["created_at"],
+        updatedAt: json["updated_at"],
+      );
+
+  toJson() {}
+}
