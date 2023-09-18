0dcf9240a59061446dcd44f7b4b859314551cd49
Hafizh <Hafizh@gmail.com>
2023-09-18 11:18:05 +0700

    Add in source API SERVICE (register, login, update image user, stat user, search user, check follow, following, no following, read follower, read following)

--- lib/source/user_source.dart
+++ lib/source/user_source.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
 import 'package:d_method/d_method.dart';
+import 'package:discuss_app/model/user.dart';
 
 import '../config/api.dart';
 import 'package:http/http.dart';
 
 class UserSource {
-  static Future<Map> register(String username, password) async {
+  static Future<Map> register(String username, String password) async {
     String url = '${Api.user}/register.php';
     try {
       Response response = await Client().post(
@@ -25,7 +26,7 @@ class UserSource {
     }
   }
 
-  static Future<Map> login(String username, password) async {
+  static Future<Map> login(String username, String password) async {
     String url = '${Api.user}/login.php';
     try {
       Response response = await Client().post(
@@ -41,6 +42,79 @@ class UserSource {
     } catch (e) {
       DMethod.printTitle('User Source - login', e.toString());
       return {'success': false};
+    }
+  }
+
+  static Future<bool> updateImage(
+    String id,
+    String oldImage,
+    String newImage,
+    String newBase64code,
+  ) async {
+    String url = '${Api.user}/update_image.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'id': id,
+          'old_image': oldImage,
+          'new_image': newImage,
+          'new_base64code': newBase64code,
+        },
+      );
+      DMethod.printTitle('User Source - updateImage', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody['success'];
+    } catch (e) {
+      DMethod.printTitle('User Source - updateImage', e.toString());
+      return false;
+    }
+  }
+
+  static Future<Map<String, dynamic>> stat(String idUser) async {
+    String url = '${Api.user}/stat.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'id_user': idUser,
+        },
+      );
+      DMethod.printTitle('User Source - stat', response.body);
+      Map<String, dynamic> responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User Source - stat', e.toString());
+      return {
+        'topic': 0.0,
+        'follower': 0.0,
+        'following': 0.0,
+      };
+    }
+  }
+
+  static Future<List<User>> search(String query) async {
+    String url = '${Api.user}/search.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'search_query': query,
+        },
+      );
+      DMethod.printTitle('User Source - search', response.body);
+      Map responseBody = jsonDecode(response.body);
+      if (responseBody['success']) {
+        List list = responseBody['data'];
+        return list.map((e) {
+          Map<String, dynamic> item = Map<String, dynamic>.from(e);
+          return User.fromJson(item);
+        }).toList();
+      }
+      return [];
+    } catch (e) {
+      DMethod.printTitle('User Source - search', e.toString());
+      return [];
     }
   }
 }
