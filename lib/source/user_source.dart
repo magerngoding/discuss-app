4a8e1d68e0ed1260c52eeb3023703b29f991b91a
Hafizh <Hafizh@gmail.com>
2023-09-18 08:44:50 +0700

    Add user source

---
+++ lib/source/user_source.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:d_method/d_method.dart';
+
+import '../config/api.dart';
+import 'package:http/http.dart';
+
+class UserSource {
+  static Future<Map> register(String username, password) async {
+    String url = '${Api.user}/register.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'username': username,
+          'password': password,
+        },
+      );
+      DMethod.printTitle('User Source - register', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User Source - register', e.toString());
+      return {'success': false};
+    }
+  }
+
+  static Future<Map> login(String username, password) async {
+    String url = '${Api.user}/login.php';
+    try {
+      Response response = await Client().post(
+        Uri.parse(url),
+        body: {
+          'username': username,
+          'password': password,
+        },
+      );
+      DMethod.printTitle('User Source - login', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User Source - login', e.toString());
+      return {'success': false};
+    }
+  }
+}
