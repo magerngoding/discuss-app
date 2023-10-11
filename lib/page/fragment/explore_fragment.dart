577a307baa51f000d093ec665a30b2c00c2a4469
Hafizh <Hafizh@gmail.com>
2023-10-11 16:07:23 +0700

    Implementation(Explore fragment)

--- lib/page/fragment/explore_fragment.dart
+++ lib/page/fragment/explore_fragment.dart
@@ -1,12 +1,78 @@
+import 'dart:convert';
+
+import 'package:d_button/d_button.dart';
+import 'package:d_view/d_view.dart';
+import 'package:discuss_app/config/app_route.dart';
+import 'package:discuss_app/controller/c_explore.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import '../../controller/c_user.dart';
+import '../../model/topic.dart';
+import '../../widget/item_topic.dart';
 
 class ExploreFragment extends StatelessWidget {
   const ExploreFragment({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Explore'),
+    final cUser = context.read<CUser>();
+    if (cUser.data == null) return DView.loadingCircle();
+    context.read<CExplore>().setTopics();
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DView.textTitle('Explore', size: 24),
+                DButtonElevation(
+                  mainColor: Theme.of(context).primaryColor,
+                  onClick: () {
+                    context.push(AppRoute.search);
+                  },
+                  height: 36,
+                  width: 36,
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: Consumer<CExplore>(
+            builder: (contextConsumer, _, child) {
+              if (_.topics.isEmpty) return DView.empty();
+
+              return ListView.builder(
+                padding: EdgeInsets.all(0),
+                itemCount: _.topics.length,
+                itemBuilder: (context, index) {
+                  Topic topic = _.topics[index];
+                  List images = jsonDecode(topic.images);
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(
+                      16,
+                      8,
+                      16,
+                      index == _.topics.length - 1 ? 30 : 8,
+                    ),
+                    child: ItemTopic(
+                      topic: topic,
+                      images: images,
+                    ),
+                  );
+                },
+              );
+            },
+          ))
+        ],
       ),
     );
   }
--- lib/widget/item_topic.dart
+++ lib/widget/item_topic.dart
@@ -23,126 +23,145 @@ class ItemTopic extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 4,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              GestureDetector(
-                onTap: () {},
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.network(
-                    '${Api.imageUser}/${topic.user!.image}',
-                    fit: BoxFit.cover,
-                    width: 30,
-                    height: 30,
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image.network(
+                      '${Api.imageUser}/${topic.user!.image}',
+                      fit: BoxFit.cover,
+                      width: 30,
+                      height: 30,
+                    ),
                   ),
                 ),
-              ),
-              DView.spaceWidth(8),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      topic.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        height: 1,
-                      ),
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        text: 'by ',
+                DView.spaceWidth(8),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        topic.title,
                         style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.grey[600],
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          height: 1,
                         ),
-                        children: [
-                          TextSpan(
-                            text: topic.user!.username,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          )
-                        ],
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: 'by ',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey[600],
+                          ),
+                          children: [
+                            TextSpan(
+                              text: topic.user!.username,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    context.push(AppRoute.detailTopic, extra: topic);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColor.primay.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    padding: EdgeInsets.fromLTRB(
+                      12,
+                      3,
+                      8,
+                      4,
+                    ),
+                    child: DView.textAction(
+                      () {},
+                      text: 'Detail',
+                      size: 14,
+                      color: Theme.of(context).primaryColor,
+                      iconRight: Icons.navigate_next,
+                      iconRightSize: 17,
+                      iconRightColor: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Text(
+              topic.description,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            if (images.isNotEmpty)
+              IntrinsicHeight(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: AspectRatio(
+                          aspectRatio: 16 / 9,
+                          child: Image.network(
+                            '${Api.imageTopic}/${images[0]}',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
                     ),
+                    if (images.length > 1)
+                      Container(
+                        width: 60,
+                        height: double.infinity,
+                        margin: const EdgeInsets.only(left: 8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.grey[300],
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          '+${images.length - 1}',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                   ],
                 ),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  color: AppColor.primay,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                padding: EdgeInsets.all(4),
-                child: DView.textAction(
-                  () {
-                    context.push(AppRoute.detailTopic, extra: topic);
-                  },
-                  text: 'Detail',
-                  size: 14,
-                  color: Theme.of(context).primaryColor,
-                  iconRight: Icons.navigate_next,
-                  iconRightSize: 17,
-                  iconRightColor: Theme.of(context).primaryColor,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          Text(topic.description),
-          SizedBox(
-            height: 8.0,
-          ),
-          if (images.isNotEmpty)
-            IntrinsicHeight(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: Image.network(
-                          '${Api.imageTopic}/${images[0]}',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  if (images.length > 1)
-                    Container(
-                      width: 60,
-                      height: double.infinity,
-                      margin: const EdgeInsets.only(left: 8),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Colors.grey[300],
-                      ),
-                      alignment: Alignment.center,
-                      child: Text(
-                        '+${images.length - 1}',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                ],
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              AppFormat.publish(
+                topic.createdAt,
               ),
             ),
-          Text(
-            AppFormat.publish(
-              topic.createdAt,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
