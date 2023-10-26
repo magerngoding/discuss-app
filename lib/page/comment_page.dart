1a8991852126fb568f63f0ce4e988b5f45502f74
Hafizh <Hafizh@gmail.com>
2023-10-26 15:12:40 +0700

    Implementation(Comment Page 2,3

--- lib/page/comment_page.dart
+++ lib/page/comment_page.dart
@@ -1,10 +1,16 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:d_button/d_button.dart';
 import 'package:d_view/d_view.dart';
+import 'package:discuss_app/config/api.dart';
 import 'package:discuss_app/config/app.color.dart';
+import 'package:discuss_app/config/app_format.dart';
 import 'package:discuss_app/controller/c_comment.dart';
+import 'package:discuss_app/controller/c_user.dart';
 import 'package:discuss_app/model/comment.dart';
+import 'package:discuss_app/source/comment_source.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 import '../model/topic.dart';
@@ -18,6 +24,10 @@ class CommenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cComment = context.read<CComment>();
+    final cUser = context.read<CUser>();
+    final controllerInput = TextEditingController();
+    cComment.setComment(topic);
     return Scaffold(
       appBar: AppBar(
         titleSpacing: 0,
@@ -53,7 +63,108 @@ class CommenPage extends StatelessWidget {
                 itemCount: _.comments.length,
                 itemBuilder: (context, index) {
                   Comment comment = _.comments[index];
-                  return SizedBox();
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(
+                      8,
+                      index == 0 ? 8 : 4,
+                      8,
+                      index == _.comments.length - 1 ? 80 : 8,
+                    ),
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.blue[50],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 18,
+                          backgroundImage: NetworkImage(
+                              '${Api.imageUser}/${comment.fromUser.image}'),
+                        ),
+                        DView.spaceWidth(8),
+                        Expanded(
+                            child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  comment.fromUser.username,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                if (comment.toUser.id != topic.idUser)
+                                  Text(
+                                    ' to',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14.0,
+                                    ),
+                                  ),
+                                if (comment.toUser.id != topic.idUser)
+                                  Text(
+                                    comment.toUser.username,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                Spacer(),
+                                Text(
+                                  AppFormat.publish(comment.createdAt),
+                                  style: TextStyle(
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            DView.spaceHeight(4),
+                            Text(comment.description),
+                            DView.spaceHeight(8),
+                            if (comment.image != '')
+                              GestureDetector(
+                                onTap: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (contextDialog) {
+                                      return Column(
+                                        children: [
+                                          DView.spaceHeight(),
+                                          DButtonCircle(
+                                            diameter: 40,
+                                            onClick: () =>
+                                                Navigator.pop(contextDialog),
+                                            child: Icon(
+                                              Icons.clear,
+                                            ),
+                                          ),
+                                          Expanded(
+                                            // Buat clik zoom
+                                            child: InteractiveViewer(
+                                              child: Image.network(
+                                                '${Api.imageComment}/${comment.image}',
+                                                fit: BoxFit.contain,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Image.network(
+                                      '${Api.imageComment}/${comment.image}'),
+                                ),
+                              )
+                          ],
+                        ))
+                      ],
+                    ),
+                  );
                 },
               );
             },
@@ -69,7 +180,7 @@ class CommenPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Consumer<CComment>(
-                    builder: (context, _, child) {
+                    builder: (contextConsumer, _, child) {
                       if (_.replyTo == null) return DView.nothing();
                       return Padding(
                         padding: EdgeInsets.fromLTRB(
@@ -99,7 +210,97 @@ class CommenPage extends StatelessWidget {
                         ),
                       );
                     },
-                  )
+                  ),
+                  Consumer<CComment>(
+                    builder: (contextConsumer, _, child) {
+                      if (_.replyTo == null) return DView.nothing();
+                      return Padding(
+                        padding: EdgeInsets.fromLTRB(8, 4, 4, 0),
+                        child: Row(
+                          children: [
+                            Text(
+                              'Image: ${_.image}',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () => cComment.pickImage(
+                          ImageSource.gallery,
+                        ),
+                        icon: Icon(
+                          Icons.image,
+                          color: Colors.white,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => cComment.pickImage(
+                          ImageSource.camera,
+                        ),
+                        icon: Icon(
+                          Icons.photo_camera,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(vertical: 4),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextField(
+                            controller: controllerInput,
+                            minLines: 1,
+                            maxLines: 5,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              isDense: true,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 8,
+                                vertical: 12,
+                              ),
+                            ),
+                            style: TextStyle(
+                              height: 1,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          CommentSource.create(
+                            topic.id,
+                            cUser.data!.id,
+                            context.read<CComment>().replyTo!.id,
+                            controllerInput.text,
+                            context.read<CComment>().image,
+                            context.read<CComment>().imageBase64code,
+                          ).then((success) {
+                            if (success) {
+                              controllerInput.clear();
+                              cComment.setComment(topic);
+                            }
+                          });
+                        },
+                        icon: Icon(
+                          Icons.check,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
